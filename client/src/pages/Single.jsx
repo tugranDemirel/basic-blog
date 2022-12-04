9f133d362a22b903fccbf2e7ed7f656155c8aaa6
Tugran Demirel <demireltugran66@gmail.com>
2022-12-04 03:26:44 +0300

    Create Pages

---
+++ client/src/pages/Single.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Single = () => {
+    return (
+        <div>Single Page</div>
+    )
+};
+
+export default Single;
---
+++ client/src/pages/Write.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Write = () => {
+    return (
+        <div>Write Page</div>
+    )
+};
+
+export default Write;
