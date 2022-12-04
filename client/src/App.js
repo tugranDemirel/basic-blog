4b393b98b874953a25fd8abf07db3d15555cd748
Tugran Demirel <demireltugran66@gmail.com>
2022-12-04 03:33:07 +0300

    usage of Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,31 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route
+} from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <div>Home page</div>
+    },
+    {
+        path: '/register',
+        element: <Register/>
+    },
+    {
+        path: '/login',
+        element: <Login/>
+    },
+])
+
+
 function App() {
   return (
     <div>
-      Hello world!
+        <RouterProvider router={router} />
     </div>
   );
 }
