ec46a22649ab53fb57c78a1fab99266642c78251
Tugran Demirel <demireltugran66@gmail.com>
2022-12-04 07:17:41 +0300

    write page design. frontend is the and

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,8 +1,60 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css'
 const Write = () => {
+    const [value, setValue] = useState('')
+    console.log(value)
     return (
-        <div>Write Page</div>
+        <div className="add">
+            <div className="content">
+                <input type="text" placeholder="title"/>
+                <div className="editorContainer">
+                    <ReactQuill className="editor" theme="snow" value={value} onChange={setValue}/>
+                </div>
+            </div>
+            <div className="menu">
+                <div className="item">
+                    <h1>Yayın</h1>
+                    <span>
+                        <b>Status: </b> Draft
+                        <b>Visibility: </b> Yayında
+                    </span>
+                    <input style={{display: 'none'}} type="file" name="" id="file"/>
+                    <label className="file" htmlFor="file">Görsel YÜkleyiniz</label>
+                    <div className="buttons">
+                        <button>Save as a draft</button>
+                        <button>Update</button>
+                    </div>
+                </div>
+                <div className="item">
+                    <h1>Kategori</h1>
+                    <div className="cat">
+                        <input type="radio" name="cat" value="art" id="art"/>
+                        <label htmlFor="art">Art</label>
+                    </div>
+                    <div className="cat">
+                        <input type="radio" name="cat" value="technology" id="technology"/>
+                        <label htmlFor="technology">Technology</label>
+                        </div>
+                    <div className="cat">
+                        <input type="radio" name="cat" value="science" id="science"/>
+                        <label htmlFor="science">Science</label>
+                    </div>
+                    <div className="cat">
+                        <input type="radio" name="cat" value="cinema" id="cinema"/>
+                        <label htmlFor="cinema">Cinema</label>
+                    </div>
+                    <div className="cat">
+                        <input type="radio" name="cat" value="design" id="design"/>
+                        <label htmlFor="art">Design</label>
+                    </div>
+                    <div className="cat">
+                        <input type="radio" name="cat" value="food" id="food"/>
+                        <label htmlFor="food">Food</label>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 };
 
